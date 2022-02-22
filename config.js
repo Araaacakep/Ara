b0c6d9161953cdd8ecdfaa2b98a13813cae9cc0d
Araaacakep <100002720+Araaacakep@users.noreply.github.com>
2022-02-22 18:38:45 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const received = chalk.bold.cyan
 const error = chalk.bold.red
 
 
-global.owner = ['6281390199407','6281393190599'] // Put your number here
+global.owner = ['085249231619','6285709141480'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = {
@@ -50,8 +50,8 @@ global.APIKeys = {
 }
 
 // Sticker WM
-global.packname = 'Nana-MD'
-global.author = 'Adul Alhy'
+global.packname = 'arabot by Tata/amrita'
+global.author = 'Tata'
 
 global.sessionid = '48736705854:2Tq6joffmVDzaS:24' // ID Sesi Instagram, Kamu Bisa Menganti Dengan Sesi Milikmu
 global.multiplier = 69 // Semakin Tinggi Angka/Nilai, Maka Peningkatan Level Akan Semakin Sulit
